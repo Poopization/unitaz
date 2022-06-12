4d1ce7724432432d43542d43f48c7be5cd497e24
Dallas <86653440+DarkJoij@users.noreply.github.com>
2022-06-12 14:00:41 +0300

    Добавил результат.

--- tests/base_example.py
+++ tests/base_example.py
@@ -1,3 +1,27 @@
 from unitazIO import Poop
 
 Poop.boom(2)
+
+
+'''
+Output:
+
+              YOU ARE HEADLESS!
+         SO MUCH!
+                      BOOM!
+              YOU ARE HEADLESS!
+         SO MUCH!
+                 BOOM!
+       YOU ARE HEADLESS!
+                          SO MUCH!
+                             YOU ARE HEADLESS!
+                         YOU ARE HEADLESS!
+Traceback (most recent call last):
+  File "tests.py", line 3, in <module>
+    Poop.boom(2)
+  File "pooper.py", line 21, in boom
+    cls.iteratively_pooped()
+  File "unitaz.py", line 10, in iteratively_pooped
+    raise UnitazIsFull('Пришло время смыть за собой!')
+unitazIO.pooping_exceptions.UnitazIsFull: Пришло время смыть за собой!
+'''
