1193110c7466ddaf864800f2cc9d227c71a0b73a
MagMigo <magmigo2@inbox.ru>
2022-06-12 19:21:13 +0800

    Кое-что обновила

--- unitazIO/pooper.py
+++ unitazIO/pooper.py
@@ -9,21 +9,18 @@ class Poop(Unitaz):
     def __init__(self, poop_times: int, flush_after_pooping: bool = False) -> None:
         self.poop_times = poop_times
         self.flush_after_pooping = flush_after_pooping
-    
-    @staticmethod
-    def spaces() -> str:
-        return ' ' * random.randint(5, 30)
+
 
     def boom(self, timer: int) -> None:
-        if timer not in range(1, 101):
-            raise TooMuchTimeForPooping('Вы не можете какать меньше 1 и дольше 100 секунд.')
+        if timer not in range(1, 1001):
+            raise TooMuchTimeForPooping('Вы не можете какать меньше 1 или дольше 1000 секунд.')
 
         words_dict = ['БУУМ!', 'ТЫ ПРОСТО В УДАРЕ!', '#ТАКМНОГО!']
         
-        for shit in range(self.poop_times):
+        for _ in range(self.poop_times):
             self.iteratively_pooped()
             time.sleep(timer)
-            print(f'{self.spaces()} {random.choice(words_dict)}')
+            print(f'{" " * random.randint(5, 30)} {random.choice(words_dict)}')
 
         if self.flush_after_pooping:
             print('\nЯ все смыл, и на всякий случай вызвал сантехника.')
