249348f3823d3d5d3e8682587867a05095282d5f
Dallas <86653440+DarkJoij@users.noreply.github.com>
2022-06-12 17:14:38 +0300

    Убрал описание вывода.

--- tests/base_example.py
+++ tests/base_example.py
@@ -3,35 +3,5 @@ from unitazIO import Poop
 poop = Poop(poop_times=5)
 poop.boom(timer=1)
 
-'''
-Вывод:
-
-              БУУМ!
-         БУУМ!
-            ТЫ ПРОТО В УДАРЕ!
-                  ТЫ ПРОСТО В УДАРЕ!
-               БУУМ!
-
-Я все смыл, и на всякий случай вызвал сантехника.
-'''
-
 poop = Poop(poop_times=15)
 poop.boom(timer=1)
-
-'''
-Вывод:
-
-                           ТЫ ПРОСТО В УДАРЕ!
-                   ТЫ ПРОСТО В УДАРЕ!
-                              ТЫ ПРОСТО В УДАРЕ!
-                  ТЫ ПРОСТО В УДАРЕ!
-                 ТЫ ПРОСТО В УДАРЕ!
-Traceback (most recent call last):
-  File "tests.py", line 19, in <module>
-    poop.boom(timer=1)
-  File "pooper.py", line 24, in boom
-    self.iteratively_pooped()
-  File "unitaz.py", line 10, in iteratively_pooped
-    raise UnitazIsFull('Пришло время смыть за собой! Вы покакали уже 10 раз подряд!')
-unitazIO.pooping_exceptions.UnitazIsFull: Пришло время смыть за собой! Вы покакали уже 10 раз подряд!
-'''
